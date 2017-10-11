60f4a65e19171feb3f65ed6ddf9630c0457b14ba
Jean <demeljean@hotmail.fr>
2017-10-11 16:30:47 +0200

    added generic comments

--- generic/first-generic.ts
+++ generic/first-generic.ts
@@ -1,12 +1,26 @@
 
-
+/*
+    On peut définir une classe générique en mettant
+    un placeholder entre chevron. Ce placeholder 
+    représente une classe réelle qui viendra le remplacer
+    partout dans la classe. 
+*/
 export class FirstGeneric<T> {
     constructor(protected prop:T){}
-
+    /**
+     * Cette méthode qui attend un paramètre de type T
+     * deviendra donc une méthode qui attend un Chien
+     * si on instancie la classe new FirstGeneric<Chien>()
+     * 
+     */
     show(parametre:T) {
         console.log(parametre);
     }
-
+    /**
+     * Cette méthode qui renvoie un tableau de T 
+     * renverra un tableau de Chien si on l'instancie 
+     * en new FirstGeneric<Chien>() 
+     */
     getEntities():T[] {
         let tableau = [];
         tableau.push(this.prop);
@@ -17,7 +31,15 @@ export class FirstGeneric<T> {
 
 }
 
-
+/**
+ * Une classe peut hériter d'une classe généric en lui
+ * spécifiant un type, ainsi, elle héritera de toutes
+ * les méthodes de la générique, mais typé comme on lui
+ * à indiquer.
+ * On peut donc définir dans la générique des méthodes
+ * communes à tous les types et définir des méthodes spécifiques
+ * à un certain type dans la classe enfant.
+ */
 export class FirstString extends FirstGeneric<string> {
     captilize():string {
         return this.prop.toUpperCase();
